cf1a1535a93dc6dd3caf45fc9cc0128953516be3
CalebeSG <calebeguimaraes10@gmail.com>
2022-05-16 15:51:29 -0300

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+export function App() {
+  return (
+    <main>
+      <h1 className="sr-only">Pricing Component</h1>
+
+      <h2 className="font-bold underline">Our Pricing</h2>
+    </main>
+  );
+}
